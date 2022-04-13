bef65ca7d622fdbb850014a73e908e75e7430e83
Flávio Arêas <vflavio9@gmail.com>
2022-04-12 21:45:37 -0300

    feat: create newsletter component

---
+++ src/components/AssinaturaNewsletter.tsx
@@ -0,0 +1,27 @@
+import mailImg from "../assets/mail.svg";
+
+export const AssinaturaNewsletter = () => {
+  return (
+    <section className="assinatura-newsletter">
+      <span>Sua casa com as</span>
+      <h2>
+        melhores
+        <br />
+        plantas
+      </h2>
+      <p>
+        Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
+        torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e
+        assine nossa newsletter para saber das novidades da marca.
+      </p>
+
+      <form>
+        <img src={mailImg} alt="" />
+
+        <input name="email" type="email" placeholder="Insira seu e-mail" />
+
+        <button type="submit">Assinar newsletter</button>
+      </form>
+    </section>
+  );
+};
